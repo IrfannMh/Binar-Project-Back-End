2e4a9232892efac202752ec84124611ffb762e0a
Ahmad Nizar <ahmad.rpl001@gmail.com>
2022-01-20 11:34:33 +0700

    style: formatting code with eslint

--- src/plugins/errorHandler.js
+++ src/plugins/errorHandler.js
@@ -1,4 +1,4 @@
-exports.onError = (err, _req, res, _next) => res.error(500, err);
+exports.onError = (err, req, res, next) => res.error(500, err);
 exports.onLost = (req, res) =>
   res.fail(404, {
     method: req.method,
